dd726737e92c5bc1d2cc974c20a9f252135c2a23
prgdanny90 <prgdanny90@gmail.com>
2018-11-24 23:34:24 +0900

    setup, helloworld

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-# expressblog
-express for blog
-
-1. npm init
-2. npm install express --save
-3. vim app.js ->
-```js
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World!');
+});
+app.get('/babo', function (req, res) {
+    res.send('you babo');
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-```
-4. node app.js
